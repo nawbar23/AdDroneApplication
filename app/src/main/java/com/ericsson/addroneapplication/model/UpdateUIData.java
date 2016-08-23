e9357bce5dbc2f81d0b02181f63c9fa4a52f4890
alcatras <klimalakamil@gmail.com>
2016-08-23 15:54:36 +0200

    Added basic communication between ControlView and ControlViewModel

---
+++ app/src/main/java/com/ericsson/addroneapplication/model/UpdateUIData.java
@@ -0,0 +1,7 @@
+package com.ericsson.addroneapplication.model;
+
+/**
+ * Created by Kamil on 8/23/2016.
+ */
+public class UpdateUIData {
+}
--- app/src/main/java/com/ericsson/addroneapplication/viewmodel/ControlViewModel.java
+++ app/src/main/java/com/ericsson/addroneapplication/viewmodel/ControlViewModel.java
@@ -1,12 +1,51 @@
 package com.ericsson.addroneapplication.viewmodel;
 
+import android.preference.PreferenceManager;
+
+import com.ericsson.addroneapplication.controller.ControlActivity;
+import com.ericsson.addroneapplication.model.UpdateUIData;
+import com.ericsson.addroneapplication.settings.SettingsActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * Created by Kamil on 8/23/2016.
  */
-public class ControlViewModel implements ViewModel {
+public class ControlViewModel implements ViewModel, ControlActivity.OnControlsChangedListener {
+
+    ControlActivity activity;
+    Timer updateTimer;
+    long delay;
+
+    private TimerTask updateTask = new TimerTask() {
+        @Override
+        public void run() {
+            UpdateUIData data = new UpdateUIData();
+
+            activity.updateUI(data);
+        }
+    };
+
+    public ControlViewModel(ControlActivity activity) {
+        this.activity = activity;
+
+        updateTimer = new Timer();
+        delay = 1000 / PreferenceManager.getDefaultSharedPreferences(activity).getInt(SettingsActivity.KEY_PREF_UI_REFRESH_RATE, 2);
+
+        resume();
+    }
+
+    public void resume() {
+        updateTimer.scheduleAtFixedRate(updateTask, 1000, delay);
+    }
+
+    public void pause() {
+        updateTimer.cancel();
+    }
 
     @Override
     public void destroy() {
-
+        updateTimer.cancel();
     }
 }
